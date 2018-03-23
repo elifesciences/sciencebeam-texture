f5581edcf61d15c2557c2448f7c7928dd14374d3
Daniel Ecer <de-code@users.noreply.github.com>
2018-03-23 17:11:36 +0000

    deploy demo website instead of default texture repo

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -17,7 +17,9 @@ const proxy = require('express-http-proxy');
 const url = require('url');
 const bodyParser = require('body-parser');
 
-const api_url = process.env.API_URL;
+const apiUrl = process.env.API_URL;
+const demoPort = process.env.DEMO_PORT || '8080';
+const nodeEnv = process.env.NODE_ENV || 'development';
 
 const path = {
   srcDir: {
@@ -164,9 +166,9 @@ gulp.task('default', ['build']);
 gulp.task('server', () => {
   if (!server) {
     server = express();
-    if (api_url) {
-      gutil.log('proxying /api to', api_url);
-      const parsedApiUrl = url.parse(api_url);
+    if (apiUrl) {
+      gutil.log('proxying /api to', apiUrl);
+      const parsedApiUrl = url.parse(apiUrl);
       const apiPath = parsedApiUrl.path;
       const apiProxy = proxy(parsedApiUrl.host, {
         proxyReqPathResolver: req => {
@@ -181,8 +183,10 @@ gulp.task('server', () => {
       gutil.log('no api url defined, not proxying api');
     }
     server.use(express.static('./dist/'));
-    server.listen('8080');
-    browserSync({proxy: 'localhost:8080', startPath: 'index.html', browser: 'google chrome'});
+    server.listen(demoPort);
+    if (nodeEnv == 'development') {
+      browserSync({proxy: 'localhost:8080', startPath: 'index.html', browser: 'google chrome'});
+    }
   } else {
     return gutil.noop;
   }
